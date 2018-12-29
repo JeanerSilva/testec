eeca4394144e03f4435ccf9d3a5f56880d3bb859
JeanerSilva <jeanerufmg@gmail.com>
2018-12-28 23:22:40 -0200

    n

--- teste.cpp
+++ teste.cpp
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
+static int function1 (int x);
 
 int main(int argc, char const *argv[])
 {
-   printf("hello world");
+   int x = function1(4);
+   int *p = &x;
+   int a[2];
+   a[0] = 4;
+   a[1] = 9;
 
+   printf("hello %d , %d, %d", *p, a[0], a[1]);
+}
+
+int function1 (int x) {
+   return x;
 }
